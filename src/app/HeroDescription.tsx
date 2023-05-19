11d899a0e2a678a9a5a79fecbab8e5927f598d42
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-19 12:38:10 +0300

    style : modify button

--- src/app/HeroDescription.tsx
+++ src/app/HeroDescription.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export default function HeroDescription() {
   return (
     <div className="flex flex-col justify-start gap-3 text-white z-10 relative">
-      <h2 className="font-semibold text-gradient flex justify-start items-center gap-3">
+      <h2 className="font-semibold text-gradient flex justify-start items-center gap-3 text-3xl">
         <div className="bg-gradient-to-r from-pink-700 to-purple-800 w-[2rem] h-[0.3rem] rounded-sm"></div>
         RASOLOMANANA Olivier
       </h2>
-      <h1 className="font-bold text-3xl-sm md:text-3xl text-gradient ">
+      <h1 className="font-bold text-5xl md:text-6xl text-gradient py-1">
         <SkillsTyping />
       </h1>
       <p className="my-3 w-full  md:w-2/3 text-gray-200">
@@ -17,10 +17,38 @@ export default function HeroDescription() {
       </p>
       <div>
         <button
-          type="button"
-          className="text-white bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          // href="#_"
+          className="relative px-10 py-3 font-medium text-white transition duration-300 bg-[#e8009b] rounded-md hover:bg-fuchsia-600 ease overflow-hidden"
         >
-          Contact me
+          <span className="absolute bottom-0 left-0 h-full -ml-2">
+            <svg
+              viewBox="0 0 487 487"
+              className="w-auto h-full opacity-100 object-stretch"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                fill="#FFF"
+                fill-rule="nonzero"
+                fill-opacity=".1"
+              ></path>
+            </svg>
+          </span>
+          <span className="absolute top-0 right-0 w-12 h-full -mr-3">
+            <svg
+              viewBox="0 0 487 487"
+              className="object-cover w-full h-full"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                fill="#FFF"
+                fill-rule="nonzero"
+                fill-opacity=".1"
+              ></path>
+            </svg>
+          </span>
+          <span className="relative">Contact me</span>
         </button>
       </div>
     </div>
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -13,10 +13,10 @@ interface Props {
 
 const ProjectCard: React.FC<Props> = ({ project }) => {
   return (
-    <div className="flex relative">
+    <div className="flex flex-col md:flex-row relative">
       <ProjectCardImage src={project.imageUrl} />
 
-      <div className="flex flex-col flex-1 md:px-6 gap-4 mb-16 md:mb-0 relative">
+      <div className="flex flex-col flex-1 py-5 md:px-6 gap-4 mb-16 md:mb-0 relative">
         <motion.h5
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -25,7 +25,7 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
           A real time chat app.
         </motion.h5>
         <motion.h2
-          className="font-bold text-[#FF00AC] text-3xl-sm"
+          className="font-bold text-[#FF00AC] text-4xl"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ type: "tween", delay: 0.2 }}
@@ -62,11 +62,25 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ type: "tween", delay: 0.4 }}
         >
-          <button
-            type="button"
-            className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border border-[#FF00AC]"
-          >
-            View demo
+          <button className="relative inline-flex items-center px-12 py-1 overflow-hidden text-lg font-medium text-[#FF00AC] border-2 border-[#FF00AC] rounded-full hover:text-white group hover:bg-slate-500">
+            <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+            <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span className="relative"> View demo</span>
           </button>
         </motion.div>
       </div>
