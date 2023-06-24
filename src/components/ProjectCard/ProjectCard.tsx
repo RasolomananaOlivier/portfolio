519583061147ad4f0677ed58b372c60d48cbdc5f
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-06-24 10:04:39 +0300

    style : change projectCard button style

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -62,25 +62,8 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ type: "tween", delay: 0.4 }}
         >
-          <button className="relative inline-flex items-center px-12 py-1 overflow-hidden text-lg font-medium text-[#FF00AC] border-2 border-[#FF00AC] rounded-full hover:text-white group hover:bg-slate-500">
-            <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-            <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-              <svg
-                className="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </span>
-            <span className="relative"> View demo</span>
+          <button className="bg-[#f010a5] hover:bg-[#c90688] text-white font-bold py-1.5 px-4 rounded-lg">
+            View Demo
           </button>
         </motion.div>
       </div>
